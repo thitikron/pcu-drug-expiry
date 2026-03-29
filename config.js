1dc1a8875789f1ea84e5ebfa0bde6b57a2d22899
thitikron <61722526+thitikron@users.noreply.github.com>
2026-03-29 17:44:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /**
  * config.js – แก้ GAS_URL หลัง Deploy
  */
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbwE0VS0vX6pczsBmYFKrnqbz81C6yqJTFcn2BJfLxY9AEKrcbjangxUa2FFiGx5Rot7/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbwgVyXxAG9MNB038twvDc-W3RZUM4UyyhnpUKhzyQpIWhXV7wfMfcIyhezmJ0eTgpBx/exec';
 // ↑ ใส่ URL จาก Google Apps Script → Deploy → Web App
